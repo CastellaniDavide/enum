97f4b8d93df1c1e6ba1dcea46117aa0ddf268525
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-07 23:21:58 +0200

    Created bin/template.c

---
+++ bin/enum.c
@@ -0,0 +1,22 @@
+/**
+ * @file enum.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief 
+ *
+ * @ingroup enum
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("enum");
+	return 0;
+}
